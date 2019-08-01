390b364ef6c592e7681bf2eaedfa8f6a7324b992
uuttff8 <sassanid18@gmail.com>
2019-08-01 12:46:12 +0300

    add yandex map

--- android/app/src/main/kotlin/com/example/sagrado_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/sagrado_flutter/MainActivity.kt
@@ -5,9 +5,12 @@ import android.os.Bundle
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 
+import com.yandex.mapkit.MapKitFactory;
+
 class MainActivity: FlutterActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    MapKitFactory.setApiKey("d1698ebd-67fd-4b72-b552-ae646583a389"); 
     GeneratedPluginRegistrant.registerWith(this)
   }
 }
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import YandexMapKit
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    YMKMapKit.setApiKey("d1698ebd-67fd-4b72-b552-ae646583a389")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
